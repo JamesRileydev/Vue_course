c0ccf4790d37f9b736c8728c5ef177fa86e069d3
JamesRileydev <jamesriley.dev@gmail.com>
2018-06-21 18:14:57 -0400

    Add damage function

--- Turn_based_game/app.js
+++ Turn_based_game/app.js
@@ -1,4 +1,4 @@
-new Vue ({
+new Vue({
   el: '#app',
   data: {
     playerHealth: 100,
@@ -6,22 +6,42 @@ new Vue ({
     gameIsRunning: false
   },
   methods: {
-    startGame: function() {
+    startGame: function () {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
     },
-    attack: function() {
+    attack: function () {
+  
+      this.monsterHealth -= this.calculateDamage(3, 10);
+
+      if (this.monsterHealth <= 0) {
+        alert("You Won!");
+        this.gameIsRunning = false;
+        return;
+      }
+
+      this.playerHealth -= this.calculateDamage(5, 12);
+
+      if (this.playerHealth <= 0) {
+        alert("You Lost!");
+        this.gameIsRunning = false;
+      }
+    },
+    specialAttack: function () {
 
     },
-    specialAttch: function() {
+    heal: function () {
+      var min = 5;
+      var max = 8;
+      var heal = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.playerHealth += heal;
+    },
+    giveUp: function () {
 
     },
-    heal: function() {
-
-    },
-    giveUp: function() {
-      
+    calculateDamage: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
     }
   }
 });
